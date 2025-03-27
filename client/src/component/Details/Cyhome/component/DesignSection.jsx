ffaf75e0e6f10caf18363ba4a29374ad9ae63806
Bearyeiz <giaphuoc.work@gmail.com>
2025-03-27 21:26:03 +0700

    Update DesignSection.jsx

--- client/src/component/Details/Cyhome/component/DesignSection.jsx
+++ client/src/component/Details/Cyhome/component/DesignSection.jsx
@@ -10,17 +10,17 @@ const DesignSection = () => {
       <div className="grid grid-cols-1 gap-10">
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/8ad88e6e2a57f25d273bab92ab0cd466d2d5b672"
-          className="w-full max-w-[945px] h-[1105px] mx-auto rounded-3xl object-center"
+          className="w-full max-w-[945px] h-[1048px] mx-auto rounded-3xl object-center"
           alt="Resident App Screens 1"
         />
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/35cff5e2d5d1a0e36242ddcb4ce5569cf95f8a64"
-          className="w-full max-w-[945px] h-[1105px] mx-auto rounded-3xl object-center"
+          className="w-full max-w-[945px] h-[1098px] mx-auto rounded-3xl object-center"
           alt="Resident App Screens 2"
         />
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/09301587a03ac81387d22af5beb5e17af52bc563"
-          className="w-full max-w-[945px] h-[1105px] mx-auto rounded-3xl object-center"
+          className="w-full max-w-[945px] h-[940px] mx-auto rounded-3xl object-center"
           alt="Resident App Screens 3"
         />
       </div>
