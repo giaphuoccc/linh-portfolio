c28e5fc95437574b1b2ce1022a0bed1c4efc1f0f
Bearyeiz <giaphuoc.work@gmail.com>
2025-05-21 00:08:17 +0700

    update client 21/05/2025

--- client/src/component/Home/component/Introduction.jsx
+++ client/src/component/Home/component/Introduction.jsx
@@ -2,7 +2,7 @@ const Introduction = () => {
   return (
     <div className="flex flex-col items-start pr-20 max-w-full rounded-none w-[861px] max-md:pr-5">
       <h1 className="text-6xl font-bold leading-tight max-md:text-4xl gradient-gold">
-        {"Linh's Porfolio"}
+        {"Linh's Portfolio"}
       </h1>
       <p className="mt-8 text-3xl leading-10 max-md:max-w-full text-[#D9C4A9]">
         Design is a journey where empathy helps us create a connection
